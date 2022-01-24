65887329b3d7c0ce24c14fb110a6679f1045bc36
Shanmathy Muruganandam <30316904+Shanmathy-Muruganandam@users.noreply.github.com>
2022-01-24 11:18:12 +0530

    Add files via upload

--- PrimeNumber.java
+++ PrimeNumber.java
@@ -4,7 +4,7 @@ public class PrimeNumber {
 	
 	public static void main(String[] args) {
 		
-		int input = 26;
+		int input = 13;
 		System.out.println("Input value: "+input);
 		boolean flag = false;
 		for(int i=2; i<=input/2; i++) {
